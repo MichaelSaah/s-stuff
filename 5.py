971ae9224f9c458b1b5f1d577385254eb0922e2b
Michael Saah <mike.saah@gmail.com>
2018-11-09 20:59:24 -0800

    modified:   5.py

--- 5.py
+++ 5.py
@@ -5,18 +5,21 @@ import numpy as np
 max_window = 240
 
 df = pandas.read_csv('pairs-reg1.csv')
-df['coeff'] = np.nan
+df['coeff'] = np.nan # initialize coeff column with NaNs
 
-for i in reversed(df.index):
+for i in reversed(df.index): # loop through indicies backwards
     ticker_pair = df.loc[i, 'tickerPair']
     
+    # set window size to 240 if possible
+    # use trading days (indicies), not calendar days
     window = 1
-    while window <= max_window and i-window > 1 and df.loc[i-window, 'tickerPair'] == ticker_pair:
+    while window < max_window and i-window > 1 and df.loc[i-window, 'tickerPair'] == ticker_pair:
         window+=1
 
     x = df.loc[i-window+1:i, 'Spread1.log']
     y = df.loc[i-window+1:i, 'Spread2.log']
-    
+
+    # this linear regression can't force a zero intercept, use something else    
     reg = stats.linregress(x, y)
     df.loc[i, 'coeff'] = reg.slope
 
