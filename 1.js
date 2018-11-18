ac2ed2f9e0c0110ad7bef74b33d386698e3c80e6
Michael Saah <mike.saah@gmail.com>
2018-11-18 15:40:07 -0800

    line breaks

--- 1.js
+++ 1.js
@@ -1,1 +1,19 @@
-var report = swan.loadAnalysis("Return and Correlation");var returnCalcs = report.get("Return Calc Options");returnCalcs.setCostBasisTreatment("Business Unit Capital");report.get("ViewChooser").setViewName("BU-SBU-Strat-Und-Sym");report.get("PortfolioSelector").setBooking(["GLEA"], ["EC", "HG", "HY", "LOANS"], ["HGNI", "HYNI"], []);report.get("ForestSelector").selectDepth(1);report.get("ForestSelector").addPaths(["root"]);var observablesInput = new Array();var observablesSWAN = new Array();var observablesReport = report.get("ObservableChooser");observablesInput.push(["JXEME Index", "JPM xs Ret", "JPM xs Ret"]);observablesInput.push(["JPDDHYI Index", "JPM US HY", "JPM US HY"]);for (var i = 0; i < observablesInput.length; i++) {      iObservable = observablesInput[i];      observablesReport.create(iObservable);      observablesSwan.push(iObservable[1]);}
+var report = swan.loadAnalysis("Return and Correlation");
+var returnCalcs = report.get("Return Calc Options");
+returnCalcs.setCostBasisTreatment("Business Unit Capital");
+report.get("ViewChooser").setViewName("BU-SBU-Strat-Und-Sym");
+report.get("PortfolioSelector").setBooking(["GLEA"], ["EC", "HG", "HY", "LOANS"], ["HGNI", "HYNI"], []);
+report.get("ForestSelector").selectDepth(1);
+report.get("ForestSelector").addPaths(["root"]);
+
+var observablesInput = new Array();
+var observablesSWAN = new Array();
+var observablesReport = report.get("ObservableChooser");
+observablesInput.push(["JXEME Index", "JPM xs Ret", "JPM xs Ret"]);
+observablesInput.push(["JPDDHYI Index", "JPM US HY", "JPM US HY"]);
+
+for (var i = 0; i < observablesInput.length; i++) {
+      iObservable = observablesInput[i];
+      observablesReport.create(iObservable);
+      observablesSwan.push(iObservable[1]);
+}
