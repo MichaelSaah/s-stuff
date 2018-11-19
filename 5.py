a79af8144d8e8a23d392634b5c2a38f4af16a929
Michael Saah <mike.saah@gmail.com>
2018-11-18 17:53:42 -0800

    finished 5 and added figure

--- 5.py
+++ 5.py
@@ -11,10 +11,10 @@ df['coeff'] = np.nan # initialize coeff column with NaNs
 for i in reversed(df.index): # loop through indicies backwards
     ticker_pair = df.loc[i, 'tickerPair']
     
-    # set window size to 240 if possible
-    # use trading days (indicies), not calendar days
+    # set window size to largest possible value <= max_window
+    # using trading days (indicies), not calendar days
     window = 1
-    while window < max_window and i-window > 1 and df.loc[i-window, 'tickerPair'] == ticker_pair:
+    while window < max_window and i-window >= 0 and df.loc[i-window, 'tickerPair'] == ticker_pair:
         window+=1
 
     x = df.loc[i-window+1:i, 'Spread1.log']
@@ -26,20 +26,33 @@ for i in reversed(df.index): # loop through indicies backwards
     reg = np.linalg.lstsq(x, y)
     df.loc[i, 'coeff'] = reg[0]
 
+    if reg[1].size > 0:
+        df.loc[i, 'resid'] = reg[1][0]
+    else:
+        df.loc[i, 'resid'] = 0 # we can do this because we're forcing 0 y-intercept
+
+# do zscores
+df['zscore'] = df.groupby('tickerPair')['resid'].transform(stats.zscore)
+
+print(df)
+
+#plotting
+
 styles = ['r-', 'b-', 'g-']
-pairs = df['tickerPair'].unique()  
+pairs = df['tickerPair'].unique()
+
+plt.subplot(2, 1, 1)
 for p, s in zip(pairs, styles):
     coeffs = df.loc[df['tickerPair'] == p]['coeff'].tolist()
     plt.plot(coeffs, s, label=p)
+    plt.title('coefficients')
+
+plt.subplot(2, 1, 2)
+for p, s in zip(pairs, styles):
+    zscores = df.loc[df['tickerPair'] == p]['zscore'].tolist()
+    plt.plot(zscores, s, label=p)
+    plt.title('z-scores')
 
 plt.legend()
+#plt.legend(bbox_to_anchor=(0, -0.2), loc=8, borderaxespad=0.)
 plt.show()
- 
-
-#xx = df.loc[0:240, 'Spread1.log']
-#yy = df.loc[0:240, 'Spread2.log']
-#plt.plot(xx, yy, 'b.')
-
-#dom = np.arange(min(xx), max(xx), 0.2)
-#plt.plot(dom, dom*df.loc[240, 'coeff'], 'r--')
-#plt.show()
